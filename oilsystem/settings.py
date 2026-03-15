e512fa3f58843b2b842b7eb6e1f521a2a26e2096
zakaria_ali <zak95469@gmail.com>
2026-03-15 04:44:31 +0200

    deploy: finalize production setup with WhiteNoise, cleaned dependencies, and Arabic font support

--- oilsystem/settings.py
+++ oilsystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-87lifvjmp*n@3cu02ozqldj*g#_miruykw@d+-kv)macb%p^z("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,7 +130,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-from django.contrib.messages import constants as messages
+# WhiteNoise storage for compressed/cached static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+from django.contrib.messages import constants as messages # type: ignore
 MESSAGE_TAGS = {
     messages.DEBUG: 'secondary',
     messages.INFO: 'info',
